a697beefd3d68a5533dbde7b2ec1d1cf27f15c87
Dan <skafdan@gmail.com>
2021-06-30 17:34:33 +1200

    printHourBin function completed

--- main.c
+++ main.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void printHourBin(int hour){
+    int i, k;
+    char* hour_ones_bits[4]; 
+    char* hour_tens_bits[2];
+    int ones = hour % 10;
+    int tens = hour / 10; 
+    printf("tens:%d ones:%d\n",tens,ones);
+    for(i = 3; i >= 0; i--){
+        k = ones >> i;
+        if(k & 1){
+            hour_ones_bits[i] = '1';
+        }else {
+            hour_ones_bits[i] = '0';
+        }
+    }
+    for(i = 1; i >= 0; i--){
+        k = tens >> i;
+        if(k & 1){
+            hour_tens_bits[i] = '1';
+        }else {
+            hour_tens_bits[i] = '0';
+        }
+    }
+    for(i = 3; i >= 0; i--){
+        printf("%c,",hour_ones_bits[i]);
+    }
+    printf("\n");
+    for(i = 1; i >= 0; i--){
+        printf("%c,",hour_tens_bits[i]);
+    }
+}
+
+void printMinBin(int min){
+
+}
+
 int main(void){
     struct tm *info;
     time_t current_time;
@@ -13,5 +49,6 @@ int main(void){
     time_as_string = ctime(&current_time);
     printf("%s\n",time_as_string);
     printf("The hour is %d\n",info->tm_hour);
+    printHourBin(info->tm_hour);
     return 0;
 }
