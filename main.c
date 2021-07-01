8e46448fa0674b3d288afaefe88792be92f39d76
Dan <skafdan@gmail.com>
2021-07-01 18:06:43 +1200

    Hour and Minute Display correctly

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void printHourBin(int hour){
     char* hour_tens_bits[2];
     int ones = hour % 10;
     int tens = hour / 10; 
-    printf("tens:%d ones:%d\n",tens,ones);
+    printf("Hour:%d Tens:%d, Ones:%d\n",hour,tens,ones);
     for(i = 3; i >= 0; i--){
         k = ones >> i;
         if(k & 1){
@@ -34,8 +34,38 @@ void printHourBin(int hour){
     }
 }
 
-void printMinBin(int min){
-
+void printMinuteBin(int minute){
+    int i,k;
+    char* minute_ones_bits[4];
+    char* minute_tens_bits[4];
+    int ones = minute % 10;
+    int tens = minute / 10;
+    printf("Minute:%d Tens:%d, Ones:%d\n",minute,tens,ones);
+    for(i = 3; i >= 0; i--){
+        k = ones >> i;
+        if(k & 1){
+            minute_ones_bits[i] = '1';
+        }
+        else {
+            minute_ones_bits[i] = '0';
+        }
+    }
+    for(i = 3; i >= 0; i--){
+        k = tens >> i;
+        if(k & 1){
+            minute_tens_bits[i] = '1';
+        }
+        else{
+            minute_tens_bits[i] = '0'; 
+        }
+    }
+    for(i = 3; i >= 0; i--){
+        printf("%c,",minute_ones_bits[i]);
+    }
+    printf("\n");
+    for(i = 3; i >= 0; i--){
+        printf("%c,",minute_tens_bits[i]);
+    }
 }
 
 int main(void){
@@ -43,12 +73,18 @@ int main(void){
     time_t current_time;
     char* time_as_string;
     
-    current_time = time(NULL);
-    info = gmtime(&current_time);
 
-    time_as_string = ctime(&current_time);
-    printf("%s\n",time_as_string);
-    printf("The hour is %d\n",info->tm_hour);
-    printHourBin(info->tm_hour);
+    while(1==1){
+        current_time = time(NULL);
+        time_as_string = ctime(&current_time);
+        printf(time_as_string);
+        info = gmtime(&current_time);
+        printHourBin(info->tm_hour);
+        printf("\n");
+        printMinuteBin(info->tm_min);
+        printf("\n");
+        sleep(1);
+        system("/usr/bin/clear");
+    }
     return 0;
 }
