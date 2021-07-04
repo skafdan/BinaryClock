4e0b0ba7e9c4cd049a249c1eea4b7c2b2fab57c7
Dan <skafdan@gmail.com>
2021-07-04 12:17:57 +1200

    Outputs with curses

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <curses.h>
+
 void printHourBin(int hour){
     int i, k;
-    char* hour_ones_bits[4]; 
-    char* hour_tens_bits[2];
+    char hour_ones_bits[4]; 
+    char hour_tens_bits[2];
     int ones = hour % 10;
     int tens = hour / 10; 
-    printf("Hour:%d Tens:%d, Ones:%d\n",hour,tens,ones);
+    printw("Hour:%d Tens:%d, Ones:%d\n",hour,tens,ones);
     for(i = 3; i >= 0; i--){
         k = ones >> i;
         if(k & 1){
@@ -26,21 +28,21 @@ void printHourBin(int hour){
         }
     }
     for(i = 3; i >= 0; i--){
-        printf("%c,",hour_ones_bits[i]);
+        printw("%c,",hour_ones_bits[i]);
     }
-    printf("\n");
+    printw("\n");
     for(i = 1; i >= 0; i--){
-        printf("%c,",hour_tens_bits[i]);
+        printw("%c,",hour_tens_bits[i]);
     }
 }
 
 void printMinuteBin(int minute){
     int i,k;
-    char* minute_ones_bits[4];
-    char* minute_tens_bits[4];
+    char minute_ones_bits[4];
+    char minute_tens_bits[4];
     int ones = minute % 10;
     int tens = minute / 10;
-    printf("Minute:%d Tens:%d, Ones:%d\n",minute,tens,ones);
+    printw("Minute:%d Tens:%d, Ones:%d\n",minute,tens,ones);
     for(i = 3; i >= 0; i--){
         k = ones >> i;
         if(k & 1){
@@ -60,11 +62,11 @@ void printMinuteBin(int minute){
         }
     }
     for(i = 3; i >= 0; i--){
-        printf("%c,",minute_ones_bits[i]);
+        printw("%c,",minute_ones_bits[i]);
     }
-    printf("\n");
+    printw("\n");
     for(i = 3; i >= 0; i--){
-        printf("%c,",minute_tens_bits[i]);
+        printw("%c,",minute_tens_bits[i]);
     }
 }
 
@@ -73,18 +75,26 @@ int main(void){
     time_t current_time;
     char* time_as_string;
     
+    initscr();
+    cbreak();
+    noecho();
+    clear();
+    refresh();
 
     while(1==1){
+        clear();
+
         current_time = time(NULL);
         time_as_string = ctime(&current_time);
-        printf(time_as_string);
+        addstr(time_as_string);
         info = gmtime(&current_time);
         printHourBin(info->tm_hour);
-        printf("\n");
+        addstr("\n");
         printMinuteBin(info->tm_min);
-        printf("\n");
+        addstr("\n");
+        refresh();
+
         sleep(1);
-        system("/usr/bin/clear");
     }
     return 0;
 }
